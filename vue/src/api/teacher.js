982bbd174b6c8482a9837e4303cbd02882d11829
mayongchao <1241979379@qq.com>
2019-01-05 14:38:50 +0800

    update vue

---
+++ vue/src/api/teacher.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+
+/** 添加教师信息 */
+export function addTeacher(data) {
+  return request({
+    url: '',
+    method: 'post',
+    params: {
+      data
+    }
+  })
+}
